d2054ee468ac9244b0b9c2b7d7adc70bd2ea2cc0
Dennis <drh89@live.com>
2019-11-20 12:39:09 +0100

    travis #7

--- src/main/java/utils/ExternalApi.java
+++ src/main/java/utils/ExternalApi.java
@@ -5,25 +5,6 @@
  */
 package utils;
 
-import com.google.gson.Gson;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonSyntaxException;
-import java.io.IOException;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Queue;
-import java.util.Scanner;
-import java.util.concurrent.ArrayBlockingQueue;
-import java.util.concurrent.Callable;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
-import java.util.concurrent.TimeoutException;
-import javafx.util.Pair;
-
 /**
  *
  * @author Dennis
